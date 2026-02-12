45874d89d5af70f9ed7a04093e379fb8c66495c9
Iasmin Santiago <76630114+iasminsantiago@users.noreply.github.com>
2026-02-12 17:13:33 -0300

    Update calculadora_TR.py

--- calculadora_TR.py
+++ calculadora_TR.py
@@ -1,3 +1,14 @@
+'''
+Calculadora simples com validação de operação.
+ Usei a variável "operacao_valida" como controle de fluxo.
+Ela serve para garantir que o resultado só seja exibido
+ se a operação for válida.
+'''
+
+# Isso é necessário porque nem todos os caminhos do código  criam a variável "resultado" (ex: divisão por zero ou operação inválida). 
+# Sem esse controle, o programa poderia tentar imprimir uma variável não definida, causando erro (NameError).
+
+
 a = float(input(" A: "))
 b = float(input("B: "))
 op = input("Operação (+ - * /): ").strip() # strip remove espaços em branco no começo ou fimd a string
