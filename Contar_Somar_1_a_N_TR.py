54dc7f158c72ecc5e61b347e18ef3e30121a9f53
Iasmin Santiago <76630114+iasminsantiago@users.noreply.github.com>
2026-02-12 18:04:36 -0300

    Create Contar_Somar_1_a_N_TR.py

---
+++ Contar_Somar_1_a_N_TR.py
@@ -0,0 +1,11 @@
+# contar de 1 a N
+n = int(input("N: "))
+for i in range(i, n+1):
+  print(i)
+
+# somar de 1 a N:
+m = int(input("N: "))
+soma = o
+for i in range (i, m+1):
+  soma += i
+print("Soma: ", soma)
