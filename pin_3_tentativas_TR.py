e3847ca1381199ce0fdbb53364d3277692cc8377
Iasmin Santiago <76630114+iasminsantiago@users.noreply.github.com>
2026-02-12 22:33:58 -0300

    Update pin_3_tentativas_TR.py

--- pin_3_tentativas_TR.py
+++ pin_3_tentativas_TR.py
@@ -8,6 +8,7 @@ liberado = False
 # Só pediremos a tentativa 3 vezes. Assim, o input deve ficar dentro do while
 # Será tentativas < 3 pois o prieiro passe foi em 0. A primeira tentativa terminou como tentativas 1. 
 # Quando rodar o terceiro passe, tentativa terminará em 3, e próximo run em while não executa mais.
+# o contador tem que estar fora do if, logo após o input, para contar a tentativa correta ou incorreta
 while tentativas <3:
   pin = int(input("Digite senha de 4 digitos: "))
   tentativas += 1
@@ -16,6 +17,7 @@ while tentativas <3:
     break
   else:
     print(f"PIN incorreto, tentativa {tentativas} de 3.")
+    
 
 print("Acesso liberado" if liberado else "Acesso bloqueado.")
 
